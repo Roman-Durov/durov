a00ecccfb3c804ca4482e286768e972ab770d1e6
Roman-Durov <83605354+Roman-Durov@users.noreply.github.com>
2021-05-03 22:49:57 +0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+﻿var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Windows';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/head-avatar.jpg') {
+        myImage.setAttribute('src', 'images/avatar-one.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/head-avatar.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Введи своё имя.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Привет, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+
+
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Привет, перейдем к изучению, ' + storedName;
+}
+
+
+myButton.onclick = function () {
+    setUserName();
+}
